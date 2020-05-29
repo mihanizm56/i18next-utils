b7b1e159f7ce7a36c246b3e51a3d3a0221dd56e2
mihanizm56 <m727507.56@mail.ru>
2020-05-29 16:57:19 +0300

    build(configure-package): upd logger

--- lib/utils/get-i18next.ts
+++ lib/utils/get-i18next.ts
@@ -6,10 +6,12 @@ type ParamsType = {
   locale: string;
 };
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 export const geti18Next = ({ appNamespace, locale }: ParamsType) =>
   new Promise(async resolve => {
     await i18next.use(ICU).init({
-      debug: true,
+      debug: isDev,
       lng: locale,
       ns: [appNamespace],
       returnEmptyString: false,
