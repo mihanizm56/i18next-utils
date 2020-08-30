0744aaca1e29018b5538475d2295cb977a4bc804
mihanizm56 <m727507.56@mail.ru>
2020-08-30 04:56:42 +0300

    build(update-package): export request url

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@ export { geti18Next } from './utils/get-i18next';
 export { getLocale } from './utils/get-locale';
 export { setLocate } from './utils/set-locale';
 export { fetchDictionary } from './utils/fetch-dictionary';
+export { i18nextRequest } from './api/i18next-request';
