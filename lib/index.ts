cfe45a3348a2082c8f80f21577ea769a93ed8b7a
mihanizm56 <m727507.56@mail.ru>
2020-09-09 01:32:40 +0300

    build(configure-package): update utils

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,3 @@ export { setLocate } from './utils/set-locale';
 export { fetchDictionary } from './utils/fetch-dictionary';
 export { i18nextRequest } from './api/i18next-request';
 export { getI18nextRequestEndpoint } from './utils/get-i18next-endpoint';
-export {
-  geti18nextProjectRequestModule,
-} from './utils/get-i18next-project-request-module';
