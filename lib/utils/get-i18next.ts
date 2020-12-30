098f38b10112409bcdbb35f73f13338a6bce7d5f
mihanizm56 <m727507.56@mail.ru>
2020-12-30 03:29:17 +0300

    build(update-package): add init resources ti init

--- lib/utils/get-i18next.ts
+++ lib/utils/get-i18next.ts
@@ -4,19 +4,24 @@ import ICU from 'i18next-icu';
 type ParamsType = {
   appNamespace: string;
   locale: string;
+  initialResources?: Record<string, any>;
 };
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-export const geti18Next = ({ appNamespace, locale }: ParamsType) =>
-  new Promise(async resolve => {
+export const geti18Next = ({
+  appNamespace,
+  locale,
+  initialResources,
+}: ParamsType) =>
+  new Promise<void>(async resolve => {
     await i18next.use(ICU).init({
       debug: isDev,
       lng: locale,
       ns: [appNamespace],
       lowerCaseLng: true,
       returnEmptyString: false,
-      resources: {},
+      resources: { ...initialResources },
     });
 
     resolve();
